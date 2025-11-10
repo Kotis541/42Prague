2fefb19eeb17422f014a920d0a887130a028cc4d
Vojtěch Kotěra <votera@seznam.cz>
2025-11-10 12:14:46 +0100

    Add new functions ft_bzero, ft_memset, and ft_memcpy; update header and include paths

--- Libft/libft.h
+++ Libft/libft.h
@@ -1,7 +1,8 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stddef.h>
+#include <stddef.h>
+#include <stdio.h>
 
 int ft_isalpha(int c);
 int ft_isdigit(int c);
@@ -9,5 +10,7 @@ int ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
 size_t ft_strlen(const char *s);
+void *ft_memset(void *s, int c, size_t n);
+void ft_bzero(void *s, size_t n);
 
 #endif
