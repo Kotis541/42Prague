829f45324a252094d2b5a5619f7e5efc9dd6d415
Vojtěch Kotěra <votera@seznam.cz>
2025-11-16 22:19:12 +0100

    Refactor Libft functions for improved readability and consistency
    - Updated ft_split, ft_strchr, ft_strdup, ft_striteri, ft_strjoin, ft_strlcat, ft_strlcpy, ft_strlen, ft_strmapi, ft_strncmp, ft_strnstr, ft_strrchr, ft_strtrim, ft_substr, ft_tolower, and ft_toupper to enhance code clarity.
    - Ensured consistent formatting and style across all modified functions.
    - Added missing function prototypes in libft.h for better function visibility.

--- Libft/libft.h
+++ Libft/libft.h
@@ -18,6 +18,12 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+typedef	struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -49,14 +55,10 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
-
-typedef	struct	s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
