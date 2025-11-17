6ce02f4bb5b933f5dcad7e322a541245fd89437c
Vojtech Kotera <vokotera@c2r10s7.42prague.com>
2025-11-17 09:46:40 +0100

    Fix null check in ft_memcpy, update ft_toupper filename, and adjust struct formatting in libft.h

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -19,7 +19,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t			i;
 
 	if (!dest && !src)
-		return (NULL)
+		return (NULL);
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
 	i = 0;
